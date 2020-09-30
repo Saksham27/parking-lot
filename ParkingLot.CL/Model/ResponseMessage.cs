119213d49fe43c5cf6678fd8df611bc396cce0e2
Saksham27 <geekysaksham27@gmail.com>
2020-09-30 16:03:25 +0530

    Added : POCO class for Response message

---
+++ ParkingLot.CL/Model/ResponseMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ParkingLot.CL.Model
+{
+  
+        /// <summary>
+        /// Poco class for Response message
+        /// </summary>
+        #region ResponseMessage
+        [DataContract]
+        public class ResponseMessage<T>
+        {
+            /// <summary>
+            /// Gets or sets Status
+            /// </summary>
+            [DataMember(Name = "Status")]
+            public bool Status { get; set; }
+
+            /// <summary>
+            /// Gets or sets Message
+            /// </summary>
+            [DataMember(Name = "Message")]
+            public string Message { get; set; }
+
+            /// <summary>
+            /// Gets or sets Data
+            /// </summary>
+            [DataMember(Name = "Data")]
+            public T Data { get; set; }
+        }
+        #endregion
+  
+}
