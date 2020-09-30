3f58f5a0917002e9bfd6da0d690dac6a2b98959d
Saksham27 <geekysaksham27@gmail.com>
2020-09-30 16:02:20 +0530

    Added : POCO class for login

---
+++ ParkingLot.CL/Model/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ParkingLot.CL.Model
+{
+    class LoginModel
+    {
+        [Required(ErrorMessage = "Username Is Required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        [RegularExpression("^.{8,15}$", ErrorMessage = "Password Length should be between 8 to 15")]
+        public string Password { get; set; }
+    }
+}
