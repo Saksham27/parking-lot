a57dfa344add4f19eb540393a9c2393d58fd09c1
Saksham27 <geekysaksham27@gmail.com>
2020-10-01 21:41:19 +0530

    Added : User interface for business layer

---
+++ ParkingLot.BL/Interface/IUserBL.cs
@@ -0,0 +1,23 @@
+﻿
+using ParkingLot.CL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.BL.Interface
+{
+    public interface IUserBL
+    {
+        ResponseMessage<ShowUserInformation> RegisterUser(UserModel data);
+
+        ResponseMessage<ShowUserInformation> LoginUser(LoginModel data);
+
+        ResponseMessage<ShowUserInformation> UpdateUser(UpdateUserInformation data, int id);
+
+        ResponseMessage<ShowUserInformation> ShowAllRegisteredUsers();
+
+        ResponseMessage<ShowUserInformation> ShowUserInformationByEmail(string email);
+
+        ResponseMessage<ShowUserInformation> DeleteUser(int id);
+    }
+}
