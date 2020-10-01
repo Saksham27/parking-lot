dc265e8da45dd93ebe749deb72b623558dd093a1
Saksham27 <geekysaksham27@gmail.com>
2020-10-01 21:40:30 +0530

    Added : User interface for repository layer

---
+++ ParkingLot.RL/Interface/IUserRL.cs
@@ -0,0 +1,23 @@
+﻿using ParkingLot.CL;
+using ParkingLot.CL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.RL.Interface
+{
+    public interface IUserRL
+    {
+        int RegisterUser(UserModel data);
+
+        int LoginUser(LoginModel data);
+
+        int UpdateUser(UpdateUserInformation data, int id);
+
+        int DeleteUser(int id);
+
+        IEnumerable<ShowUserInformation> ShowAllRegisteredUsers();
+
+        IEnumerable<ShowUserInformation> ShowUserInformationByEmail(string email);
+    }
+}
