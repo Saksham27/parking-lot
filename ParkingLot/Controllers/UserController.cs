6f22b0dcee1a2bf0be9824dc8b9b7bf81c0239f9
Saksham27 <geekysaksham27@gmail.com>
2020-10-01 21:35:53 +0530

    Added : user controller

--- ParkingLot/Controllers/UserController.cs
+++ ParkingLot/Controllers/UserController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ParkingLot.BL.Interface;
+using ParkingLot.CL.Model;
 
 namespace ParkingLot.Controllers
 {
@@ -11,5 +14,129 @@ namespace ParkingLot.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private IConfiguration configuration;
+        IUserBL userBusinessLayer;
+        ResponseMessage<ShowUserInformation> response;
+
+        public UserController(IUserBL userBL, IConfiguration config)
+        {
+            userBusinessLayer = userBL;
+            configuration = config;
+        }
+
+        [HttpGet]
+        [Route("allUsers")]
+        public ActionResult<IEnumerable<string>> ShowRegisteredUsers()
+        {
+            try
+            {
+                response = userBusinessLayer.ShowAllRegisteredUsers();
+
+                if(response.Status == true)
+                {
+                    return Ok(new { response.Status, response.Message, response.Data });
+                }
+                else
+                {
+                    return BadRequest(new { response.Status, response.Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(new { error = exception.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public ActionResult UserRegistration([FromBody] UserModel model)
+        {
+            try
+            {
+                response = userBusinessLayer.RegisterUser(model);
+                if (response.Status == true)
+                {
+                    return this.Ok(new { response.Status, response.Message });
+                }
+                else
+                {
+                    return BadRequest(new { response.Status, response.Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(new { error = exception.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public ActionResult UserLogin([FromBody] LoginModel model)
+        {
+            try
+            {
+                response = userBusinessLayer.LoginUser(model);
+                if (response.Status == true)
+                {
+                    return this.Ok(new { response.Status, response.Message });
+                }
+                else
+                {
+                    return BadRequest(new { response.Status, response.Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(new { error = exception.Message });
+            }
+        }
+
+        [HttpPatch]
+        [Route("update/{id}")]
+
+        public ActionResult UpdateUserDetails([FromBody] UpdateUserInformation data,[FromRoute] int id)
+        {
+            try
+            {
+                response = userBusinessLayer.UpdateUser(data,id);
+                if (response.Status == true)
+                {
+                    return Ok(new { response.Status, response.Message });
+                }
+                else
+                {
+                    return BadRequest(new { response.Status, response.Message });
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(new { error = exception.Message });
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public ActionResult DeleteUser([FromRoute] int id)
+        {
+            try
+            {
+                response = userBusinessLayer.DeleteUser(id);
+                if (response.Status == true)
+                {
+                    return Ok(new { response.Status, response.Message });
+                }
+                else
+                {
+                    return BadRequest(new { response.Status, response.Message });
+                }
+            }
+            catch (Exception exception)
+            {
+
+                return BadRequest(new { error = exception.Message });
+
+            }
+        }
+
     }
 }
