d4cb59620a89bc93ba00a0674d9829abf96caa52
Saksham27 <geekysaksham27@gmail.com>
2020-10-12 16:09:28 +0530

    Added : Poco class for ticket generated after parking

---
+++ ParkingLot.CL/Model/ParkingTicket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ParkingLot.CL.Model
+{
+    public class ParkingTicket
+    {
+        [DataMember(Name = "TicketId")]
+        [Required(ErrorMessage = "Ticket Id Is Required")]
+        [RegularExpression(" ^[1-9][0-9]*$", ErrorMessage = "Ticket id is not valid")]
+        public int TicketId { get; set; }
+
+        [DataMember(Name = "VehicleNumber")]
+        [Required(ErrorMessage = "Vehicle Number Is Required")]
+        [RegularExpression("^[A-Z][a-zA-Z]{3,15}$", ErrorMessage = "Vehicle Number is not valid")]
+        public string VehicleNumber { get; set; }
+
+        [DataMember(Name = "ParkingSlot")]
+        public string ParkingSlot { get; set; }
+
+        public string EntryTime { get; set; }
+
+    }
+}
