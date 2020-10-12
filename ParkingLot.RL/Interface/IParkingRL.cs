445b60e3349f602d2b74e782f89147c00a458c29
Saksham27 <geekysaksham27@gmail.com>
2020-10-12 16:15:19 +0530

    Added : Parking interface for repository layer

---
+++ ParkingLot.RL/Interface/IParkingRL.cs
@@ -0,0 +1,14 @@
+﻿
+using ParkingLot.CL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.RL.Interface
+{
+    public interface IParkingRL
+    {
+        Tuple<string,ParkingTicket> ParkVehicle(ParkVehicleModel vehcileDetails);
+        Tuple<string,ParkingBill> UnparkVehicle(int ticketId);
+    }
+}
