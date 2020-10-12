cfa2f19be8057bcd4ffe4e93631d99d793fa6343
Saksham27 <geekysaksham27@gmail.com>
2020-10-12 16:17:06 +0530

    Added : parking interface for business layer

---
+++ ParkingLot.BL/Interface/IParkingBL.cs
@@ -0,0 +1,13 @@
+﻿using ParkingLot.CL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.BL.Interface
+{
+    public interface IParkingBL
+    {
+        ResponseMessage<ParkingTicket> ParkVehicle(ParkVehicleModel vehicleDetails);
+        ResponseMessage<ParkingBill> UnparkVehicle(int ticketId);
+    }
+}
