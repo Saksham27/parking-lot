b15f252ec8de22dbaccf6aa001d5faf5928ef207
Saksham27 <geekysaksham27@gmail.com>
2020-10-12 16:09:58 +0530

    Added : Poco class for Bill generated after unparking

---
+++ ParkingLot.CL/Model/ParkingBill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ParkingLot.CL.Model
+{
+    public class ParkingBill
+    {
+
+        [DataMember(Name = "VehicleNumber")]
+        public string VehicleNumber { get; set; }
+
+        public string EntryTime { get; set; }
+
+        public string ExitTime { get; set; }
+
+        public string ParkingCost { get; set; }
+
+    }
+}
