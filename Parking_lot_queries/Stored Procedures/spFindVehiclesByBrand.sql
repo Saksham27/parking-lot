454ed31f15acf2273b169a952425e106c3ed5cbf
Saksham27 <geekysaksham27@gmail.com>
2020-10-13 18:23:07 +0530

    Added : stored procedure for finiding vehicles of given brand

---
+++ Parking_lot_queries/Stored Procedures/spFindVehiclesByBrand.sql
@@ -0,0 +1,22 @@
+USE [ParkingLotDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- -----------------------------------------------------------------------
+-- Author : Saksham Singh
+-- description : Stored procedure for getting all users details
+-- ----------------------------------------------------------------------
+create PROCEDURE [dbo].[spFindVehiclesByBrand]
+@brand varchar(50)
+AS
+BEGIN
+	Select VehicleNumber, VehicalBrand,
+	VehicalColor, ParkingSlot, ParkingStatus, EntryTime, ExitTime,
+	FirstName, LastName, UserName, EmailID, 
+	Address, UserRole, Handicapped, RegistrationDate 
+	from (ParkingLot JOIN RegisteredUsers
+	ON ParkingLot.VehicleOwner = RegisteredUsers.ID) where VehicalBrand=@brand
+	
+END
